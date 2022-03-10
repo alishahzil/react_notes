23604bea56f28a2f62293813a566ead65716764c
alishahzil <syedalishahzil@gmail.com>
2022-03-10 11:35:07 +0500

    react_js

---
+++ React_notes.js
@@ -0,0 +1,17 @@
+/// React coding notes by Syed Ali Shahzil
+
+
+
+// useState
+
+//add new property into object of useState
+
+ const [state, setState] = useState({
+    name: '',
+    age: '',
+ })
+
+ setState(...currentdata,{})
+
+
+//useEffect
