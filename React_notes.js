54ae55e371455ad704e4f217dc404a0cce508562
Ali Shahzil <52203884+alishahzil@users.noreply.github.com>
2022-03-22 01:20:59 +0500

    Update React_notes.js

--- React_notes.js
+++ React_notes.js
@@ -15,3 +15,38 @@
 
 
 //useEffect
+
+
+
+
+
+
+
+//Problem
+
+//how to call a child component's function from its parent
+
+const Parent = () => {
+  const childFunc = React.useRef(null)
+
+  return (
+    <>
+      <Child childFunc={childFunc} />
+      <button onClick={() => childFunc.current()}>Click me</button>
+    </>
+  )
+}
+
+const Child = ({ childFunc }) => {
+  React.useEffect(() => {
+    childFunc.current = alertUser
+  }, [])
+
+  function alertUser() {
+    alert('You clicked!')
+  }
+
+  return null
+}
+
+render(<Parent />)
